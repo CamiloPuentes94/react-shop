e2b7955825e8a6dd36122e18559aee7be36da232
Camilo Puentes <jcpsandoval94@gmail.com>
2022-09-09 15:48:35 -0500

    Se  realiza las rutas con Browser route, Switch y Route

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword'
@@ -8,11 +9,17 @@ import '../styles/global.css';
 
 const App = () => {
   return(
-    <Layout>
-      <Login />
-      <RecoveryPassword />
-    </Layout>
-  );
+    <BrowserRouter>
+    <Switch>
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/recovery-password" component={RecoveryPassword} />
+        <Route component={NotFound} />
+      </Layout>
+    </Switch>
+    </BrowserRouter>
+  );  
 }
 
 export default App;
