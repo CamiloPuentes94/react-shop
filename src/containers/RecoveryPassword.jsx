cb16048796a90bb99035118ab22439c62716165c
Camilo Puentes <jcpsandoval94@gmail.com>
2022-09-09 15:34:19 -0500

    Se import estilos del recovery y se verifica la semantica

--- src/containers/RecoveryPassword.jsx
+++ src/containers/RecoveryPassword.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
+import '../styles/RecoveryPassword.scss'
 
 const RecoveryPassword = () => {
   return(
   <div class="login">
     <div class="form-container">
       <img src="./logos/logo_yard_sale.svg" alt="logo" class="logo" />
-
       <h1 class="title">Email has been sent!</h1>
       <p class="subtitle">Please check your inbox for instructions on how to reset the password</p>
-
       <div class="email-image">
         <img src="./icons/email.svg" alt="email" />
       </div>
-
       <button class="primary-button login-button">Login</button>
-
       <p class="resend">
         <span>Didn't receive the email?</span>
         <a href="/">Resend</a>
