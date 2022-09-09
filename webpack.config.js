505eadf86c997edcec929fc7fdb81391c273360d
Camilo Puentes <jcpsandoval94@gmail.com>
2022-09-09 13:33:44 -0500

    Se cambia regla para que acepte archivos css

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         }        
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(css|scss)$/,
         use:[
           "style-loader",
           "css-loader",
