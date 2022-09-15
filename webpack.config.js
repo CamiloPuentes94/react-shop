cdf8ecf81a1594e0b7ca4681f37c02250b3a1470
Camilo Puentes <jcpsandoval94@gmail.com>
2022-09-15 16:04:29 -0500

    Se agregan la lectura de los archivos de imagenes y los alias del proyecto

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
   mode: 'development',
   resolve: {
     extensions:['.js', '.jsx'],
+    alias:{
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@containers': path.resolve(__dirname, 'src/containers/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+      '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+    }
   },
   module:{
     rules:[
@@ -35,6 +43,10 @@ module.exports = {
           "css-loader",
           "sass-loader",
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type:'asset'
       }
     ]
   },
