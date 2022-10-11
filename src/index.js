61fcbdde6be7f16cf7484f5cd900fb479e8f040a
Camilo Puentes <jcpsandoval94@gmail.com>
2022-10-11 08:01:08 -0500

    Se agrgea warning de react dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Se importa react
 import React from 'react';
 import  ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 // Se importa el componente de react
 import App from './routes/App';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+createRoot(document.getElementById('app')).render(<App />)
