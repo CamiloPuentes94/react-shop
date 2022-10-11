25fe5e3c1fa925221fbc3a3c5603134b2cd7944c
Camilo Puentes <jcpsandoval94@gmail.com>
2022-10-10 22:45:56 -0500

    Se realiza useState para el toggle del menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/Header.scss';
+import Menu from '@components/Menu'
 
 import icon_menu from'@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
@@ -7,6 +8,13 @@ import icon_shopping_cart from '@icons/icon_shopping_cart.svg';
 
 
 const Header = () => {
+
+  const[toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  }
+
   return (
     <nav>
       <img src={icon_menu} alt="menu" className="menu" />
@@ -35,13 +43,17 @@ const Header = () => {
       </div>
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">platzi@example.com</li>
+          <li className="navbar-email" onClick={ handleToggle }>
+            platzi@example.com
+          </li>
           <li className="navbar-shopping-cart">
             <img src={icon_shopping_cart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
+      
     </nav>
   );
 }
