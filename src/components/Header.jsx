fde379b50279a26ac27598a586ef13138d9784d5
Camilo Puentes <jcpsandoval94@gmail.com>
2022-10-11 08:44:30 -0500

    Se realiza funcion para la apertura y cierre de myOrder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
-import Menu from '@components/Menu'
-import AppContext from '@context/AppContext'
+import Menu from '@components/Menu';
+import AppContext from '@context/AppContext';
+import MyOrder  from '@containers/MyOrder';
 
 import icon_menu from'@icons/icon_menu.svg';
 import logo_yard_sale from '@logos/logo_yard_sale.svg';
@@ -11,6 +12,7 @@ import icon_shopping_cart from '@icons/icon_shopping_cart.svg';
 const Header = () => {
 
   const[toggle, setToggle] = useState(false);
+  const[toggleOrders, setToggleOrders] = useState(false)
   const {state} = useContext(AppContext);
 
   const handleToggle = () => {
@@ -48,14 +50,17 @@ const Header = () => {
           <li className="navbar-email" onClick={ handleToggle }>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
+          <li 
+            className="navbar-shopping-cart" 
+            onClick={() => setToggleOrders(!toggleOrders)}
+          >
             <img src={icon_shopping_cart} alt="shopping cart" />
             {state.cart.length > 0 ? <div>{state.cart.length}</div> : null }
           </li>
         </ul>
       </div>
       {toggle && <Menu />}
-      
+      {toggleOrders && <MyOrder />}
     </nav>
   );
 }
