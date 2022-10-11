be8966378b80accb1e6cb889b35f53c7badde02e
Camilo Puentes <jcpsandoval94@gmail.com>
2022-10-11 07:59:22 -0500

    Se hace cambio de la escritura del async await para evitar warning en la aplciacion

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -5,10 +5,14 @@ const useGetProducts= (API) => {
 
 	const[products, setProducts] = useState([]);
 	
-	useEffect(async() => {
-    const response = await axios(API);
-    setProducts(response.data);
+	useEffect(() => {
+    getData();
   },[]); 
+  const getData = async() =>{
+    const response =await axios(API);
+    setProducts(response.data);
+    console.log(response);
+  }
   return products;
 };
 
